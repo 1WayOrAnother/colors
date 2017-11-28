89693be75b30bbe0929fbe11256469ddf5606fa0
Kyle Tenzeldam and Paul Koto <kyle@>
2017-11-28 10:45:04 -0800

    set up buttons to change the bg color of our page

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").css("background-color", "green");
+});
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").css("background-color", "yellow");
+  });
+
+    $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").css("background-color", "red");
+  });
+});
